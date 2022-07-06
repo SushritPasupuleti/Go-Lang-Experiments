f8c64e3be9b0a3b8a5c5ae4db7620edf085e24cb
Sushrit_Lawliet <sushrit.pk21@gmail.com>
2022-07-06 20:48:58 +0530

    misc: strings

--- hello.go
+++ hello.go
@@ -1,10 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 //outside of main function, variables must be properly declared
-var gloabl_i float32 = 10
-var External_i int = 20 //exposed outside package
+var globalI float32 = 10
+var ExternalI int = 20 //exposed outside package
 
 //All variables with a starting lowercase character in name are not exposed outside of package
 
@@ -27,12 +30,32 @@ func main() {
 	fmt.Println("Hello There", i, x)
 	fmt.Printf("Val: %v Type: %T\n", i, i)
 	fmt.Printf("Val: %v Type: %T\n", x, x)
-	fmt.Printf("Val: %v Type: %T\n", gloabl_i, gloabl_i)
+	fmt.Printf("Val: %v Type: %T\n", globalI, globalI)
 	fmt.Printf("Val: %v Type: %T\n", a, a)
 	fmt.Printf("Val: %v Type: %T\n", b, b)
 	fmt.Printf("Val: %v Type: %T\n", c, c)
 
 	i = i + 1
 	fmt.Printf("Val: %v Type: %T\n", i, i)
-	fmt.Printf("Val: %v Type: %T\n", External_i, External_i)
+	fmt.Printf("Val: %v Type: %T\n", ExternalI, ExternalI)
+
+	//type coercion
+
+	c = float32(c)
+
+	fmt.Printf("Type Casted 'c' Val: %v Type: %T\n", c, c)
+
+	//working with strings
+
+	s := "Hello There"
+	fmt.Println(s)
+
+	var d = strconv.Itoa(i)	//convert int to string
+
+	fmt.Printf("Val: %v Type: %T\n", d, d)
+
+	sb := []byte(s)	//convert string to byte slice
+
+	fmt.Printf("Val: %v Type: %T\n", sb, sb)
+	//prints them as ASCII value array.
 }
